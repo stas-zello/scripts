7d74946436b3f313c2fc9e917ae6ec3326070fa9
Stanislaw Razynski <stanislaw@Stanislaws-MBP.lan>
2023-03-03 17:10:38 -0500

    Adding changes which parse .json files
    Also added 2 examples of input files which can be used to generate
    current configs.

--- conf_parser.py
+++ conf_parser.py
@@ -1,10 +1,24 @@
 import sys
+import json
 from collections import defaultdict
+
 delimeter = "`"
+
 def parse_file(input_file):
+    file_dictionary = {}
+    try:
+        json_data = json.load(input_file)
+        file_dictionary = parse_json(json_data)
+    except:
+        input_file.seek(0)
+        file_dictionary = parse_text_file(input_file)
+    return file_dictionary
+
+def parse_text_file(input_file):
     file_dictionary = {}
     lines = input_file.readlines()
     for line in lines:
+        print(line)
         if line[0]=="#":
             continue
         else:
@@ -13,7 +27,13 @@ def parse_file(input_file):
                 file_dictionary[tokens[0]] = "blank"
             elif len(tokens) == 2:
                 file_dictionary[tokens[0]] = tokens[1]
+    print(file_dictionary)
+    return file_dictionary
 
+def parse_json(json_data):
+    file_dictionary = {}
+    for key in json_data.keys():
+        file_dictionary[key] = str(json_data[key])
     return file_dictionary
 
 def merge_configurations(file_dictionaries):
@@ -22,11 +42,9 @@ def merge_configurations(file_dictionaries):
         all_key_dictionary = {}
         for file_name, a_dictionary in file_dictionaries:
             first_line.append(file_name)
-            print(first_line)
             all_key_dictionary = {**all_key_dictionary,**a_dictionary}
         output_file.write(delimeter.join(first_line)+"\n")
         key_list = list(all_key_dictionary.keys())
-        print(key_list)
         all_configs = defaultdict(list)
         for file_name, a_dictionary in file_dictionaries:
             for config_key in key_list:
@@ -40,9 +58,25 @@ def add_configs_to_file(all_configs, output_file):
     for key in all_configs:
         output_file.write(key + delimeter + delimeter.join(all_configs[key])+"\n")
     return
+
+def create_input_file_list(input_filename):
+    with open(input_filename, "r") as input_file:
+        return input_file.readlines() 
+    
 def main():
+    print("Usage /`python3 conf_parser.py <input file listing .conf files> <output file name> <sperator character (default `)>")
+    input_filename = sys.argv[1:2][0]
+    output_filename = sys.argv[2:3][0]
+    if len(sys.argv) == 4:
+        delimeter = sys.argv[3:4][0]
+        print("Seperator specified: " + delimeter)
+
+    print("Analyzing files listed in: " + input_filename)
+    print("Outputting to: " + output_filename)
+    file_list = create_input_file_list(input_filename)
     file_dictionaries = []
-    for file_name in sys.argv[1:]:
+    for file_name in file_list:
+        file_name = file_name.rstrip()
         with open(file_name) as input_file:
             file_dictionaries.append((file_name,parse_file(input_file)))
     merge_configurations(file_dictionaries)
