51a5be69809bb9c41fec41c38c98042dc895f9d9
Stanislaw Razynski <stanislaw@Stanislaws-MacBook-Pro.local>
2023-03-06 17:02:10 -0500

    New .gitignore to ignore interim files
    sn_conf_list.txt update to remove invalid JSON file
    conf_parse changed to generate wiki compatible formatting

--- conf_parser.py
+++ conf_parser.py
@@ -3,22 +3,24 @@ import json
 from collections import defaultdict
 
 delimeter = "`"
+output_filename = "total_config"
 
 def parse_file(input_file):
     file_dictionary = {}
     try:
         json_data = json.load(input_file)
         file_dictionary = parse_json(json_data)
+        print(input_file.name + " processed as JSON")
     except:
         input_file.seek(0)
         file_dictionary = parse_text_file(input_file)
+        print(input_file.name + " processed as TEXT")
     return file_dictionary
 
 def parse_text_file(input_file):
     file_dictionary = {}
     lines = input_file.readlines()
     for line in lines:
-        print(line)
         if line[0]=="#":
             continue
         else:
@@ -27,7 +29,6 @@ def parse_text_file(input_file):
                 file_dictionary[tokens[0]] = "blank"
             elif len(tokens) == 2:
                 file_dictionary[tokens[0]] = tokens[1]
-    print(file_dictionary)
     return file_dictionary
 
 def parse_json(json_data):
@@ -37,22 +38,33 @@ def parse_json(json_data):
     return file_dictionary
 
 def merge_configurations(file_dictionaries):
-    with open("total_config.csv","w") as output_file:
-        first_line = ["Parameter name"]
-        all_key_dictionary = {}
-        for file_name, a_dictionary in file_dictionaries:
-            first_line.append(file_name)
-            all_key_dictionary = {**all_key_dictionary,**a_dictionary}
+    first_line = ["Parameter name"]
+    all_key_dictionary = {}
+    for file_name, a_dictionary in file_dictionaries:
+        first_line.append(file_name)
+        all_key_dictionary = {**all_key_dictionary,**a_dictionary}
+    key_list = list(all_key_dictionary.keys())
+    all_configs = defaultdict(list)
+    for file_name, a_dictionary in file_dictionaries:
+        for config_key in key_list:
+            if config_key in a_dictionary:
+                all_configs[config_key].append(a_dictionary[config_key])
+            else:
+                all_configs[config_key].append("NOT IN CONFIG")
+    global output_filename
+    with open(output_filename+".csv","w") as output_file:
         output_file.write(delimeter.join(first_line)+"\n")
-        key_list = list(all_key_dictionary.keys())
-        all_configs = defaultdict(list)
-        for file_name, a_dictionary in file_dictionaries:
-            for config_key in key_list:
-                if config_key in a_dictionary:
-                    all_configs[config_key].append(a_dictionary[config_key])
-                else:
-                    all_configs[config_key].append("NOT IN CONFIG")
         add_configs_to_file(all_configs, output_file)
+    with open(output_filename+".md","w") as wiki_file:
+        wiki_file.write("|".join(first_line)+"\n")
+        for item in first_line:
+            wiki_file.write("| --- ")
+        wiki_file.write("|\n")
+        add_configs_to_wiki(all_configs, wiki_file)
+
+def add_configs_to_wiki(all_configs, wiki_file):
+    for key in all_configs:
+        wiki_file.write(key+"|"+"|".join(all_configs[key])+"\n")
 
 def add_configs_to_file(all_configs, output_file):
     for key in all_configs:
@@ -66,8 +78,10 @@ def create_input_file_list(input_filename):
 def main():
     print("Usage /`python3 conf_parser.py <input file listing .conf files> <output file name> <sperator character (default `)>")
     input_filename = sys.argv[1:2][0]
+    global output_filename 
     output_filename = sys.argv[2:3][0]
     if len(sys.argv) == 4:
+        global delimeter
         delimeter = sys.argv[3:4][0]
         print("Seperator specified: " + delimeter)
 
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python3 conf_parser.py ls-cloud_conf_list.txt ls-cloud_conf
+python3 conf_parser.py sn_conf_list.txt sn_conf
